1f7876c0ef5ea4227bbc46f132182c48863f0384
minumsa <icycyi@naver.com>
2022-10-03 18:40:00 +0900

    combineReducers, Redux Provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,21 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
-import counter from './reducers'
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(counter);
+const store = createStore(rootReducer);
+
+store.dispatch({
+  type: "ADD_TODO",
+  text: "USE REDUX"
+})
+console.log('store.getState', store.getState());
 
 const render = () =>  root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App
       value={store.getState()}
       onIncrement={()=> store.dispatch({type:"INCREMENT"})}
       onDecrement={()=> store.dispatch({type:"DECREMENT"})}
     />
+    </Provider>
   </React.StrictMode>
 );
 render();
